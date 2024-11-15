f7df71839f6f5490c49227eda948412c97f45054
SantiReRo <santiago.reyes.rodriguez@correounivalle.edu.co>
2024-11-15 13:06:58 -0500

    eliminar para restauracion

--- run.sh
+++
@@ -1,6 +0,0 @@
-#PYTHONPATH=. python3 busquedas_no_informadas/Profundidad.py
-#PYTHONPATH=. python3 busquedas_no_informadas/Amplitud.py
-#PYTHONPATH=. python3 busquedas_no_informadas/costoUniforme.py
-#PYTHONPATH=. python3 busquedas_no_informadas/Profundidad_iterativa.py
-#PYTHONPATH=. python3 busquedas_no_informadas/limitada_profundidad.py
-PYTHONPATH=. python3 main.py
