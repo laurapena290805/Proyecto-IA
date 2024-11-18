563fd9e1064e94a0b87c2be1258ea4e63b9c52c7
lauracoicue <coicue.laura@correounivalle.edu.co>
2024-11-18 15:38:05 -0500

    Refactorizar método de eliminación de nodos en la clase GraficarArbol

--- Arbol/graficar_arbol.py
+++ Arbol/graficar_arbol.py
@@ -20,6 +20,16 @@ class GraficarArbol:
 
         plt.show()
 
+    def eliminar_nodos(self, nodos):
+        if nodos == []:
+            return
+        for nodo in nodos:
+            self.graph.remove_node((nodo.fila, nodo.columna, nodo.id))
+        self.pos = self.hierarchy_pos(self.graph, self.root)
+        nx.draw(self.graph, self.pos, with_labels=True, node_size=500, node_color="orange", font_size=10)
+        plt.savefig("arbol.png")
+        plt.show()
+
     def hierarchy_pos(self, G, root=None, width=1., vert_gap=0.2, vert_loc=0, xcenter=0.5):
         pos = self._hierarchy_pos(G, root, width, vert_gap, vert_loc, xcenter, parsed=set())
         return pos
